a05d071458845766bc83533cf560c9df50fb48eb
abidibo <abidibo@gmail.com>
2011-07-05 18:31:51 +0200

    Removed some page module unused functions

--- modules/page/page.php
+++ modules/page/page.php
@@ -5,14 +5,12 @@ class page extends model {
 	function __construct($id) {
 	
 		parent::__construct($this->initP());
-
-		if($id=='home_public_summary') $this->homePageSummary();
 	
 	}
 
 	private function initP() {
 
-		return array("title"=>null, "text"=>null, "image"=>null);
+		return array();
 
 	}
 
