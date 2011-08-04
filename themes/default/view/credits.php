13ebae31b8a69890cc3aef2c3cf40a67951601b9
abidibo <abidibo@gmail.com>
2011-08-04 16:15:38 +0200

    Restyle of default and white themes, logo and favicon. Added gitignore.

--- themes/default/view/credits.php
+++ themes/default/view/credits.php
@@ -1,1 +1,2 @@
+<div></div>
 <?= $registry->dtime->now(); ?> | Jeff Php Framework, <?= __('poweredby') ?> <a href="http://www.otto.to.it">Otto srl</a> 
--- themes/default/view/layout_list.php
+++ themes/default/view/layout_list.php
@@ -1,17 +1,17 @@
 <h1 class="section_title"><?= $title ?></h1>
 <p><?= $text ?></p>
-<table class="generic" style="width:100%">
+<table class="generic wide">
 	<tr>
 		<th style="width:130px"><?= __("Preview") ?></th><th><?= __("Name") ?></th><th><?= __("Description") ?></th><th class="noBorder noBkg"></th>
 	</tr>
 <? foreach($items as $item): ?>
-	<tr>
+	<tr<?= $item['active'] ? " class=\"selected\"" : "" ?>>
 		<td style="text-align:center">
 			<a href="<?= $item['image'] ?>" title="<?= $item['name'] ?>" rel="lightbox"><img style="height:60px" src="<?= $item['image'] ?>" alt="screenshot"/></a>
 		</td>
 		<td><?= $item['name'] ?></td>
 		<td><?= $item['description'] ?></td>
-		<td class="noBorder"><?= $item['active'] ? __("ACTIVE") : "<input type='button' value='".__("activate")."' onclick=\"location.href='".$item['link_activate']."'\" />" ?></td>
+		<td class="noBorder noBkg" style="padding:23px 0 0 10px;"><?= $item['active'] ? "" : "<input type='button' value='".__("activate")."' onclick=\"location.href='".$item['link_activate']."'\" />" ?></td>
 	</tr>
 <? endforeach ?>
 </table>
