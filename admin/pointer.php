32985ecb0aa7db34074e4ed11209010339fbd2b0
abidibo <abidibo@gmail.com>
2011-10-21 15:12:40 +0200

    Added pointer in admin folder
    So that admin ajax calls set the right ROOT_ADMIN constant and the system remains in the admin site.

---
+++ admin/pointer.php
@@ -0,0 +1,15 @@
+<?php
+
+define('ABS_ROOT', realpath(dirname(dirname(__FILE__))));
+define( 'DS', DIRECTORY_SEPARATOR );
+
+include(ABS_ROOT.DS.'paths.php');
+include(ABS_ROOT.DS.'configuration.php');
+include(ABS_CORE.DS.'core.php');
+
+define('BASE_PATH', ROOT_ADMIN);
+
+$core = new core();
+$core->methodPointer();
+
+?>
